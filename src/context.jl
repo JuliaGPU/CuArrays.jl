815d49ba28c4e7c56b7b07675bf58ee7b4c0fbba
Dhairya Gandhi <dhairya@juliacopmuting.com>
2019-08-20 00:19:14 +0530

    use context/ rm iddict

--- src/context.jl
+++ src/context.jl
@@ -1,9 +1,32 @@
-using IRTools: isexpr, IR, @dynamo
-using IRTools: meta, Pipe, finish, Variable
+using IRTools: isexpr, IR, @dynamo, postwalk
+using IRTools: meta, Pipe, finish, Variable, self
+using MacroTools: @forward
 
 import Base.Broadcast.broadcasted
 import Base.Broadcast.materialize
 import Base.Broadcast.Broadcasted
+
+struct IRCtx{T,K}
+  array_bank::IdDict{T,K}
+
+  IRCtx() = new{Array, CuArray}(IdDict{Array,CuArray}())
+end
+
+# Display fns for debugging, remove before committing
+function Base.summary(io::IO, c::IRCtx)
+  print(io, "IR Context for CUDA ")
+  summary(io, c.array_bank)
+end
+
+function Base.show(io::IO, c::IRCtx{T,K}) where {T,K}
+  print(io, "IR Context for CUDA ")
+  display(c.array_bank)
+end
+
+@forward IRCtx.array_bank Base.getindex, Base.iterate,
+			Base.setindex!, Base.empty!,
+			Base.length,
+			Base.first, Base.last, Base.haskey
 
 function _resize!(a::Array, sz::NTuple{<:Any,Integer})
   ccall(:jl_array_grow_end, Cvoid, (Any, UInt), a, prod(sz))
@@ -25,14 +48,12 @@ function cache(cx, x::CuArray{T,N})::Array{T,N} where {T,N}
   return cpu
 end
 
-# Hold all the objects related to the op
-# obs = IdDict()
-
 for f in (:+, :-, :*, :/)
-  @eval function cuize(::typeof($f), a::Array, b::Array)
+  @eval function (c::IRCtx)(::typeof($f), a::AbstractArray, b::AbstractArray)
     ga = get_cached(array_bank, a)
     gb = get_cached(array_bank, b)
-    cache(array_bank, $f(ga, gb))
+    # cache(array_bank, $f(ga, gb))
+    $f(ga, gb)
   end
 end
 
@@ -42,26 +63,38 @@ function get_cached(array_bank, arr::Array{T,N})::CuArray{T,N} where {T,N}
     (array_bank[arr] = CuArray(arr))
 end
 
-function cuize(::typeof(broadcasted), f, args...)
+function (c::IRCtx)(::typeof(broadcasted), f, args...)
   gargs = map(x -> get_cached(array_bank, x), args)
-  Main.broadcasted(f, gargs...)
+  broadcasted(f, gargs...)
 end
 
-function cuize(::typeof(getproperty), o, s::Symbol)
+function (c::IRCtx)(::typeof(getproperty), o, s::Symbol)
   getproperty(o, s) |> get_cached
 end
 
-# function cuize(::typeof(getproperty), o, s::Symbol)
-#   getproperty(get_cached(o), s)
-# end
-
-function cuize(::typeof(broadcast), f, args...)
-  @show f
+function (c::IRCtx)(::typeof(broadcast), f, args...)
   gargs = map(x -> get_cached(array_bank, x), args)
   broadcast(f, gargs...)
 end
 
-@dynamo function cuize(meta...)
+function (c::IRCtx)(::typeof(getfield), o, s::Symbol)
+  getfield(o, s) |> get_cached
+end
+
+function wrap_cuize(f)
+  @eval function (c::IRCtx)(::typeof($f), args...)
+    gargs = map(get_cached, args)
+    $f(gargs...) # use `cache`
+  end
+end
+
+wrap_cuize.((sum, similar, ))
+
+function (c::IRCtx)(::typeof(reshape), arr, args...)
+  reshape(get_cached(arr), args...)
+end
+
+@dynamo function (c::IRCtx)(meta...)
   meta == nothing && return
   ir = IR(meta...)
   ir == nothing && return
@@ -71,43 +104,16 @@ end
     isexpr(st.expr, :call) || continue
     ex = st.expr
 
-    pr[v] = Expr(:call, GlobalRef(CuArrays, :cuize), ex.args...)
+    pr[v] = Expr(:call, self, ex.args...)
 
   end
   return finish(pr)
 end
 
-cuize(::typeof(setindex!), ::Tuple, args...) = tuple(args...)
-
-###################################################################
-
-function children(x::T, fs = fieldnames(T)) where T
-  (; zip(fs, map(f -> get_cached(getproperty(x, f)), fs))...)
-end
-
-children(x::Tuple) = map(get_cached, x)
-# function children(s::T) where T <:AbstractSet
-#   t = T()
-#   for p in s
-#     push!(t, get_cached(p))
-#   end
-#   t
-# end
-
-
-# mapchildren(x::T) where T = @eval $(Symbol(T.name))($(children(x))...)
-mapchildren(x::T) where T = children(x)
-# mapchildren(x::T) where T<:AbstractSet = children(x)
-
-function get_cached(obs, x::T) where T
-  haskey(obs, x) && return obs[x]
-  x isa CuArray && return x
-
-  obs[x] = mapchildren(x)
-end
-
-get_cached(array_bank, t::Union{Type, Function, Broadcasted, Symbol, Module, Nothing, Missing, Ptr, T}) where {T <: Real} = t
+get_cached(array_bank, t::Union{Type, UnitRange, Function, Broadcasted, Symbol, Module, Nothing, Missing, Ptr, CuPtr, T}) where {T <: Real} = t
 get_cached(array_bank, t::Union{Tuple, NamedTuple}) = map(get_cached, t)
+
+get_cached(array_bank, x::CuArray) = x
 
 function get_cached(x::T) where T
   T <: Array && return get_cached(array_bank, x)
@@ -116,42 +122,49 @@ function get_cached(x::T) where T
 end
 
 """
-  Disable `cuize` for a function
+  Disable `IRCtx` for a function
 """
-function noop_pass(f, args...)
-  @eval cuize(::typeof($f), args...) = $f(args...)
+function noop_pass(f)
+  @eval (c::IRCtx)(::typeof($f), args...) = $f(args...)
 end
 
-noop_pass.((materialize, ))
+noop_pass.((materialize, get_cached, NNlib.check_spdf,
+	))
 
-cuize(::typeof(get_cached), args...) = get_cached(args...)
-
-function makechildren(T::Type, nt::NamedTuple)
-  eval(nameof(T))(nt...)
-end
-
-
-# Functions called inside `cuize` aren't executed as part of the context
-# So any assumptions made inside the context (`getproperty`, for eg) will
-# not Hold
-# Thus we need the actual objects when trying to call the objects, as opposed to
-# continuing inside the context where we can pick fields up from a NamedTuple
-# Without this limitation, we can avoid caching the structs themselves
-function cuize(::typeof(invoke), f::T, types, args...) where T
-  gf = f isa Function ? f : makechildren(T, get_cached(obs, f))
-  invoke(gf, types, map(get_cached, args)...)
+for f in names(NNlib)
+  getfield(NNlib, f) isa Function || continue
+  @eval function (c::IRCtx)(::typeof($f), args...)
+    gargs = map(get_cached, args)
+    # cache(array_bank, $f(gargs...))
+    $f(gargs...)
+  end
 end
 
 # Hold all the arrays related to the op
 # TODO: this should be a context
-const array_bank = IdDict{Array,CuArray}()
+# BitArray and friends would like an AbstractArray construct
+# const array_bank = IdDict{Array,CuArray}()
 
+const array_bank = IRCtx()
+
+"""
+  Creates a `cuda` context within which we travel
+  through the entire callstack to find matrix/vector
+  operations and try to offload them to a GPU.
+
+  Example:
+  ```
+  cuda() do
+    # do something
+  end
+  ```
+"""
 function cuda(f)
-  out = cuize(f)
+  out = array_bank(f)
   for (x, cx) in array_bank
     length(x) == length(cx) && continue
     refill!(x, cx)
   end
-  empty!(array_bank)
+  # empty!(array_bank)
   return out
 end
