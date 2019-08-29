50eaec920a596b81c29d1fab2910efc5aff05ef4
Dhairya Gandhi <dhairya@juliacopmuting.com>
2019-08-29 19:44:13 +0530

    add basic tests

--- test/context.jl
+++ test/context.jl
@@ -1,6 +1,46 @@
 using CuArrays, Test
+using CuArrays.NNlib
 
-W = rand(5, 5)
-b = rand(5)
+# Check simple ops work and broadcast
+@testset "simple ops" begin
+  W = rand(5, 5)
+  b = rand(5)
+  @test cuda(() -> W*b) ≈ W*b
 
-@test cuda(() -> W*b) ≈ W*b
+  a = rand(10)
+  b = rand(10)
+
+  r = cuda() do
+    a + b
+  end
+  @test r isa Array
+
+  r = cuda() do
+    a .+ b
+  end
+  @test r isa Array
+end
+
+# Check that functions happen
+@testset "linear" begin
+  linear(x, W, b) = (x * W) .+ b
+  w = rand(10, 10)
+  b = zeros(10)
+  x = rand(10,10)
+  r = cuda() do
+    linear(x, w, b)
+  end
+  @test r isa Array{Float32}
+end
+
+# check that NNlib is wrapped correctly
+@testset "conv Context" begin
+  w = rand(Float32, 3, 3, 3, 16)
+  r = rand(Float32, 32, 32, 3, 1)
+  g = cuda() do
+    conv(r, w)
+  end
+  g = conv(r, w)
+  @test c ≈ g
+  @test g isa Array
+end
